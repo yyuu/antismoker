f464066c0b83f891d6ee0555ec093074f9cbab22
Jon Rowett <jon@jonrowett.com>
2015-10-15 14:44:32 +0100

    turned off SSL verification

--- lib/antismoker/tests/https.rb
+++ lib/antismoker/tests/https.rb
@@ -10,6 +10,29 @@ module AntiSmoker
     def uri
       URI::HTTPS.build(:host => host, :port => port, :path => path)
     end
+
+    def fetch(uri, options={})
+      limit = options.fetch(:limit, 10)
+      method = options.fetch(:method, "GET")
+      raise(ArgumentError.new("HTTP redirect too deep")) if limit <= 0
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      case method
+      when /^post$/i
+        response = http.post(uri.request_uri.to_s, data)
+      else
+        response = http.get(uri.request_uri.to_s)
+      end
+      if Net::HTTPRedirection === response
+        location = URI.parse(response["location"])
+        new_uri = (location.absolute?) ? location : uri.merge(location)
+        fetch(new_uri, :limit => limit-1)
+      else
+        response
+      end
+    end
   end
 end
-
